a6b3a44fa0f031bce3d6f707a297c5848ff54a4f
Serikzhan <tlekkaliserik@icloud.com>
2023-12-07 15:21:11 +0500

    add account management features

--- equipify/urls.py
+++ equipify/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.generic import RedirectView
+from django_email_verification import urls as email_urls
 
 favicon_view = RedirectView.as_view(
     url='/static/images/favicon.ico', permanent=True)
@@ -10,6 +11,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls', namespace='shop')),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('account/', include('account.urls', namespace='account')),
+    path('email/', include(email_urls), name='email-verification'),
     re_path(r'^favicon\.ico$', favicon_view),
 ]
 
